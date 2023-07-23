adbffa8a1eef58104c795538af2364faa8c95d18
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-07-23 16:49:19 +0900

    회원 저장

---
+++ src/main/java/hello/servlet/web/frontController/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,28 @@
+package hello.servlet.web.frontController.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.v3.ControllerV3;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paraMap) {
+        String username = paraMap.get("username");
+        int age = Integer.parseInt(paraMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member",member);
+        return mv;
+    }
+}
